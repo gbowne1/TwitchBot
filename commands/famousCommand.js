dd0e3205a2f5da2b0bbefadcc82f391bf790c45c
Greg Bowne <gbowne144@gmail.com>
2023-05-04 16:53:39 -0700

    Adding necessary packages and a new command to ban famous bots

---
+++ commands/famousCommand.js
@@ -0,0 +1,11 @@
+// commands/famousCommand.js
+
+module.exports = async function famousCommand(twitchClient, channel) {
+	twitchClient.on('message', (channel, tags, message, self) => {
+		if (self) return; // Ignore messages from the bot
+
+		if (message.match(/famous/)) {
+			twitchClient.ban(channel, tags['username']);
+		}
+	});
+};
