c14c2100f9cd62aa9a99977b5aca6a6d5bf94aef
Amrani-Farouk-Hossam-Eddine <119071864+Amrani-Farouk-Hossam-Eddine@users.noreply.github.com>
2024-02-22 10:49:39 +0100

    add routes protection

--- src/client/src/components/DrawerMenu.js
+++ src/client/src/components/DrawerMenu.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { Link } from 'react-router-dom';
-import { routes } from '../routes';
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Link } from "react-router-dom";
+import { routes } from "../routes";
 
 const DrawerMenu = (props) => {
-    const navRoutes = routes.filter((route) => route.inNav);
-    return (
-        <Drawer anchor='left' open={props.isOpen} onClose={props.onClose}>
-            <List>
-                {navRoutes.map((route, index) => (
-                    <ListItem
-                        key={index}
-                        sx={{ color: 'palette.text.primary' }}
-                    >
-                        <Link to={route.path} style={{ color: 'inherit' }}>
-                            <ListItemText primary={route.label} />
-                        </Link>
-                    </ListItem>
-                ))}
-            </List>
-        </Drawer>
-    );
+  const navRoutes = routes.filter((route) => route.inNav);
+  return (
+    <Drawer anchor="left" open={props.isOpen} onClose={props.onClose}>
+      <List>
+        {navRoutes.map((route, index) => (
+          <ListItem key={index} sx={{ color: "palette.text.primary" }}>
+            <Link to={route.path} style={{ color: "inherit" }}>
+              <ListItemText primary={route.label} />
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
 };
 
 export default DrawerMenu;
-
