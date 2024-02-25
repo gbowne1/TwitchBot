8f77e77302753ea2dafb5b3f8c64cc189fa50a9e
Jonathan Armentor <jonathan@jonathanarmentor.dev>
2024-02-25 13:54:12 -0800

    Resolve linting errors

--- src/server/commands/lurk.js
+++ src/server/commands/lurk.js
@@ -1,5 +1,5 @@
 // commands/lurk.js
 module.exports = async function lurk(twitchClient, channel, userstate) {
   const username = userstate.username;
-  twitchClient.say(channel, `\${username} is now lurking. Enjoy your lurk!`);
+  twitchClient.say(channel, `${username} is now lurking. Enjoy your lurk!`);
 };
