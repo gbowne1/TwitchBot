5413649d6a75f171c19d107feb048ae66fa205f1
Jonathan Armentor <jonathan@jonathanarmentor.dev>
2024-02-25 08:29:29 -0800

    Add eslint and prettier to server.
    Add eslint configuration and prettier configuration.
    Add quick-lint-js configuration
    Add scripts to package.json

---
+++ src/server/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    curly: ["error", "all"],
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1, maxBOF: 0 }],
+  },
+};
