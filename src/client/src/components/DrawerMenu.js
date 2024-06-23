e97992b0aeaecfda840c1fdba382289efcc4fe00
Babuprasanth R <babuprasanth.r2727@gmail.com>
2024-06-23 13:06:07 +0530

    First Commit

--- src/client/src/components/DrawerMenu.js
+++ src/client/src/components/DrawerMenu.js
@@ -3,6 +3,8 @@ import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { routes } from "../routes";
@@ -10,17 +12,22 @@ import { useTheme } from "@mui/material/styles";
 
 const DrawerMenu = (props) => {
   const navRoutes = routes.filter((route) => route.inNav);
-
   const theme = useTheme();
 
   return (
     <Drawer anchor="left" open={props.isOpen} onClose={props.onClose}>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '8px' }}>
+        <IconButton onClick={props.onClose}>
+          <CloseIcon />
+        </IconButton>
+      </div>
       <List>
         {navRoutes.map((route, index) => (
           <ListItem key={index} sx={{ color: theme.palette.text.primary }}>
             <Link
               to={route.path}
               style={{ color: "inherit", textDecoration: "none" }}
+              onClick={props.onClose}
             >
               <ListItemText primary={route.label} />
             </Link>
